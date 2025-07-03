2c24656e547f3c1e749379ae2f6df981e7f7eaf4
Younes Menasri <thisizneo.dev@gmail.com>
2025-07-03 09:37:54 +0100

    Gallexter v0.1 - backend setup

---
+++ GalleryApp/README.md
@@ -0,0 +1,19 @@
+### Gallexter - WebApp
+# Description
+Web Application that i made to Learn New Features Like Uploading Files (images).
+
+# Thechnolgies Used
+
+-NodeJS[expressJS]
+-ReactJS
+-MONGODB
+-REDUX
+-TailwindCss
+
+```
+Please Dont Ruined the App uplaod only some pictures
+```
+
+# Preview
+
+<img src="Gallexter.png" />
---
+++ GalleryApp/backend/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+// ---
+import dotenv from "dotenv"
+dotenv.config();
+
+// IMPORTING ROUTES
+import postRouter from './routes/posts.js';
+
+const PORT = process.env.PORT || 5300;
+const app = express();
+
+// INITI ROUTES
+app.use('/api', postRouter)
+
+// Init libraries
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// SERVER STARTING...
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Listening on: http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
