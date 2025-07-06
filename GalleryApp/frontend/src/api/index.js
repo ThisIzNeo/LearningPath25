a7ed19bd4dab395aeef5161b7b596fedfea58ab8
Younes Menasri <thisizneo.dev@gmail.com>
2025-07-06 09:40:22 +0100

    v0.9 - Forntend-backend completing (posting and fetching)

--- GalleryApp/frontend/src/api/index.js
+++ GalleryApp/frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: 'http://localhost:5239/api' });
+const API = axios.create({ baseURL: 'https://gallexter.onrender.com/api' });
 
 export const fetchGallery = () => API.get('/data');
 
